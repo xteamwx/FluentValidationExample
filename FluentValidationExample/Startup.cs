fd4c75e9700c9e3593f51207066c77aaa1e4e0ad
wangx <xteam_wx@hotmail.com>
2021-03-18 15:40:35 +0800

    添加项目文件。

---
+++ FluentValidationExample/Startup.cs
@@ -0,0 +1,81 @@
+using FluentValidation.AspNetCore;
+using FluentValidationExample.Model;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FluentValidationExample
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers()
+           .AddFluentValidation(conf =>           //添加模型验证器
+           {
+               //
+               conf.RegisterValidatorsFromAssemblyContaining<IValidation>();
+               conf.RunDefaultMvcValidationAfterFluentValidationExecutes = false;
+           });
+
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.InvalidModelStateResponseFactory = (context) =>
+                {
+                    var errors = context.ModelState
+                        .Values
+                        .SelectMany(x => x.Errors
+                                    .Select(p => p.ErrorMessage))
+                        .ToList();
+
+                    var result = new
+                    {
+                        isSuccess = false,
+                        code = "0009",
+                        message = "Validation errors",
+                        response = errors
+                    };
+
+                    return new BadRequestObjectResult(result);
+                };
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
